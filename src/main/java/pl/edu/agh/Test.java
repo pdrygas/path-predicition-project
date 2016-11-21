348870945e636d400beacae17d5bba07bb035228
Bloomin117 <bloomin@o2.pl>
2016-11-21 12:40:37 +0100

    initial commit with basic sample

---
+++ src/main/java/pl/edu/agh/Test.java
@@ -0,0 +1,58 @@
+package pl.edu.agh;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+
+//
+// Detects faces in an image, draws boxes around them, and writes the results
+// to "faceDetection.png".
+//
+class DetectFaceDemo {
+    public void run() {
+        System.out.println("\nRunning DetectFaceDemo");
+
+        String xmlPath = getClass().getResource("/lbpcascade_frontalface.xml").getPath();
+        String pngPath = getClass().getResource("/lena.png").getPath();
+
+
+        // Create a face detector from the cascade file in the resources
+        // directory.
+        CascadeClassifier faceDetector = new CascadeClassifier(new File(xmlPath).getAbsolutePath());
+        Mat image = Highgui.imread(new File(pngPath).getAbsolutePath());
+
+        // Detect faces in the image.
+        // MatOfRect is a special container class for Rect.
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image, faceDetections);
+
+        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+        // Draw a bounding box around each face.
+        for (Rect rect : faceDetections.toArray()) {
+            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+        }
+
+        // Save the visualized detection.
+        String filename = "faceDetection.png";
+        System.out.println(String.format("Writing %s", filename));
+        Highgui.imwrite(filename, image);
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, OpenCV");
+
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new DetectFaceDemo().run();
+    }
+}
