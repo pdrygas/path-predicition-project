d8646f8e0e2d4fc1c68024448d8a2dbe87269025
Pawel Drygas <pawel.drygas@salesgossip.co.uk>
2016-12-08 19:06:55 +0100

    Added new feature points detection

--- src/main/java/pl/edu/agh/FeaturePointsTracker.java
+++ src/main/java/pl/edu/agh/FeaturePointsTracker.java
@@ -4,11 +4,21 @@ import org.opencv.core.*;
 import org.opencv.video.Video;
 
 public class FeaturePointsTracker {
-    public MatOfPoint2f trackFeaturePoints(Mat prevFrame,Mat currFrame, MatOfPoint2f prevFPoints) {
+    public MatOfPoint2f trackFeaturePoints(Mat prevFrame,Mat currFrame, FoundEntity entity,FeaturePointsFinder finder, boolean t) {
+        Rect box = entity.getBoundingBox();
+        MatOfPoint2f prevFPoints;
+        if (t) {
+            prevFPoints = finder.findFeaturePoints(new Mat(prevFrame, box), new Point(box.x, box.y));
+        } else {
+            prevFPoints = entity.getFPoints();
+        }
         MatOfPoint2f newFPoints = new MatOfPoint2f();
         MatOfByte results = new MatOfByte();
         MatOfFloat errors = new MatOfFloat();
 
+        if (prevFPoints == null) {
+            return null;
+        }
         Video.calcOpticalFlowPyrLK(prevFrame, currFrame, prevFPoints, newFPoints, results, errors);
 
         return  newFPoints;
