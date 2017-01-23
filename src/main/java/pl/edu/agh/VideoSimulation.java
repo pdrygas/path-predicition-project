663ecd9baf406860baf8a8018db2533b73c7cd83
Bloomin117 <bloomin@o2.pl>
2017-01-23 22:46:00 +0100

    Refactoring

--- src/main/java/pl/edu/agh/VideoSimulation.java
+++ src/main/java/pl/edu/agh/VideoSimulation.java
@@ -30,129 +30,119 @@ public class VideoSimulation {
     private HeatMap heatMap;
     private Rect detectionArea;
     private Size resizedMatSize;
-    private ArrayList<java.awt.Point> points;
+    private CascadeClassifier classifier;
     private boolean playVideo;
 
     public VideoSimulation(String videoFile, HeatMap heatMap, Rect detectionArea) {
         videoPath = getClass().getResource("/" + videoFile).getPath();
         xmlPath = getClass().getResource("/haarcascade_fullbody.xml").getPath();
+        classifier = new CascadeClassifier(new File(xmlPath).getAbsolutePath());
         scaleFactor = 1.01;
         minNeighbors = 1;
         minSize = new Size(0, 30);
-        maxSize = new Size(50, 70);
+        maxSize = new Size(50, 65);
 
         gui = new GUI();
-        points = null;
         resizedMatSize = new Size(250,600);
         playVideo = true;
 
         this.heatMap = heatMap;
         this.detectionArea = detectionArea;
 
+        createButton();
+        run();
+    }
+
+    private void createButton() {
         Button btn = new Button();
         btn.setLabel("Stop");
         btn.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                if(playVideo) {
+                    btn.setLabel("Start");
+                } else {
+                    btn.setLabel("Stop");
+                }
                 playVideo = !playVideo;
             }
         });
         gui.add(btn);
-
-        run();
     }
 
-    public void run() {
-        CascadeClassifier classifier = new CascadeClassifier(new File(xmlPath).getAbsolutePath());
+    private void run() {
         VideoCapture video = new VideoCapture(new File(videoPath).getAbsolutePath());
-        Mat matToShow;
         Mat mat = new Mat();
         Mat prevMat = new Mat();
 
         boolean searched = false;
-        int j = 0;
 
-        video.read(mat);
+        video.read(prevMat);
         for(;;) {
             System.out.println(""); // MAGIC NOP
             if(playVideo) {
                 if(video.grab()) {
                     searched = false;
-                    if(j > 5) {
-                        prevMat = mat;
-                        j = 0;
-                    }
+                    prevMat = mat;
 
                     video.retrieve(mat);
                     mat = new Mat(mat, detectionArea);
-
-//                    if (points != null) {
-//                        for (int i = 0; i < points.size() - 1; i++) {
-//                            Core.line(mat, new Point(points.get(i).x * heatMap.getxStep(), points.get(i).y * heatMap.getyStep()),
-//                                    new Point(points.get(i + 1).x * heatMap.getxStep(), points.get(i + 1).y * heatMap.getyStep()),
-//                                    new Scalar(0, 0, 255));
-//                        }
-//                    }
-
-                    Mat resizedMat = new Mat();
-                    matToShow = mat;
-                    Imgproc.resize(matToShow, resizedMat, resizedMatSize);
-                    gui.show(resizedMat);
+                    drawMat(mat);
                     try {
                         Thread.sleep(1000 / 25);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                   j++;
                 }
             } else {
                 if(!searched) {
+                    MatOfRect foundPrev = detectPeople(new Mat(prevMat, detectionArea));
+                    MatOfRect foundCurrent = detectPeople(mat);
+
                     searched = true;
+                    predictPaths(foundPrev, foundCurrent, mat);
+                    drawMat(mat);
+                }
+            }
+        }
+    }
 
-                    MatOfRect foundPrev = new MatOfRect();
-                    MatOfRect foundCurrent = new MatOfRect();
+    private void drawMat(Mat mat) {
+        Mat resizedMat = new Mat();
+        Imgproc.resize(mat, resizedMat, resizedMatSize);
+        gui.show(resizedMat);
+    }
 
-                    Mat tmpMat = new Mat(prevMat, detectionArea);
-                    classifier.detectMultiScale(tmpMat, foundPrev, scaleFactor, minNeighbors, 0, minSize, maxSize);
-                    classifier.detectMultiScale(mat, foundCurrent, scaleFactor, minNeighbors, 0, minSize, maxSize);
+    private void drawPath(Point cellPosition, Simulation.Direction direction, Scalar color, Mat mat) {
+        Simulation sim = new Simulation(heatMap, (int) cellPosition.x, (int) cellPosition.y, direction);
+        ArrayList<java.awt.Point> points = sim.getPath();
+        if (points != null) {
+            for (int i = 0; i < points.size() - 1; i++) {
+                Core.line(mat, new Point(points.get(i).x * heatMap.getxStep(), points.get(i).y * heatMap.getyStep()),
+                               new Point(points.get(i + 1).x * heatMap.getxStep(), points.get(i + 1).y * heatMap.getyStep()),
+                               color, 2);
+            }
+        }
+    }
 
+    private MatOfRect detectPeople(Mat mat) {
+        MatOfRect result = new MatOfRect();
+        classifier.detectMultiScale(mat, result, scaleFactor, minNeighbors, 0, minSize, maxSize);
+        return result;
+    }
 
+    private void predictPaths(MatOfRect foundPrev, MatOfRect foundCurrent, Mat mat) {
+        final int MAX_X_DIFF = 3;
 
-                    final double diff = 3;
-                    for (Rect oldRect : foundPrev.toArray()) {
-                        for(Rect newRect : foundCurrent.toArray()) {
-                            if(Math.abs(oldRect.x - newRect.x) < diff) {
-                                Core.rectangle(mat, new Point(newRect.x, newRect.y), new Point(newRect.x+newRect.width, newRect.y+newRect.height), new Scalar(100));
-//                                Core.rectangle(mat, new Point(oldRect.x, oldRect.y), new Point(oldRect.x+oldRect.width, oldRect.y+oldRect.height), new Scalar(0, 160, 0));
+        for (Rect oldRect : foundPrev.toArray()) {
+            for(Rect newRect : foundCurrent.toArray()) {
+                // Check if person appeared in both frames
+                if(Math.abs(oldRect.x - newRect.x) < MAX_X_DIFF) {
+                    Core.rectangle(mat, new Point(newRect.x, newRect.y), new Point(newRect.x+newRect.width, newRect.y+newRect.height), new Scalar(100));
 
-                                Point cellPosition = heatMap.cellPosition(new Point(newRect.x+ newRect.width / 2,  newRect.y + newRect.height / 2));
-
-                                Simulation sim = new Simulation(heatMap, (int) cellPosition.x, (int) cellPosition.y, Simulation.Direction.NORTH);
-                                points = sim.getPath();
-                                if (points != null) {
-                                    for (int i = 0; i < points.size() - 1; i++) {
-                                        Core.line(mat, new Point(points.get(i).x * heatMap.getxStep(), points.get(i).y * heatMap.getyStep()),
-                                                new Point(points.get(i + 1).x * heatMap.getxStep(), points.get(i + 1).y * heatMap.getyStep()),
-                                                new Scalar(0, 0, 255), 2);
-                                    }
-                                }
-                                sim = new Simulation(heatMap, (int) cellPosition.x, (int) cellPosition.y, Simulation.Direction.SOUTH);
-                                points = sim.getPath();
-                                if (points != null) {
-                                    for (int i = 0; i < points.size() - 1; i++) {
-                                        Core.line(mat, new Point(points.get(i).x * heatMap.getxStep(), points.get(i).y * heatMap.getyStep()),
-                                                new Point(points.get(i + 1).x * heatMap.getxStep(), points.get(i + 1).y * heatMap.getyStep()),
-                                                new Scalar(0, 255, 255), 2);
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    Mat resizedMat = new Mat();
-                    matToShow = mat;
-                    Imgproc.resize(matToShow, resizedMat, resizedMatSize);
-                    gui.show(resizedMat);
+                    Point cellPosition = heatMap.cellPosition(new Point(newRect.x + newRect.width/2,  newRect.y + newRect.height/2));
+                    drawPath(cellPosition, Simulation.Direction.NORTH, new Scalar(0, 0, 255), mat);
+                    drawPath(cellPosition, Simulation.Direction.SOUTH, new Scalar(0, 255, 255), mat);
                 }
             }
         }
